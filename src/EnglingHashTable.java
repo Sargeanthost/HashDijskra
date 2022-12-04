523b6bc6bd52290c4a754202c13a7a85b3347512
Sargeanthost <sargeanthost@gmail.com>
2022-12-04 17:00:44 -0500

    hashtable should be finished

--- src/EnglingHashTable.java
+++ src/EnglingHashTable.java
@@ -1,29 +1,41 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class EnglingHashTable {
     private final int size;
 
-
     private int occupied;
-    private String[] array;
+    private final String[] array;
+    private final int[] hashOccurrenceArray;
+    private final HashMap<String, Integer> wordOffsetList = new HashMap<>();
 
     public EnglingHashTable(int size) {
         this.size = size;//293
         array = new String[size];
+        hashOccurrenceArray = new int[size];
     }
 
     public void insert(String element) {
         int hash = hashCode(element);
-        while (!(array[hash % size] == null)) {//circular indexing
-            hash++;
+        int index = hash;
+        int distance = 0;
+        hashOccurrenceArray[hash]++;
+
+        while (!(array[index % size] == null)) {//circular indexing
+            index++;
+            distance++;
             //error handling
-            if (hash == 2 * size) {
+            if (index == 2 * size) {
                 System.out.println("Array Filled!");
                 break;
             }
         }
-        array[hash % size] = element;
+        array[index % size] = element;
         occupied++;
+        wordOffsetList.put(element, distance);
     }
 
     private int hashCode(String element) {
@@ -34,18 +46,82 @@ public class EnglingHashTable {
         return (float) occupied / size;
     }
 
-    public String getIndex(int index){
+    public String getIndex(int index) {
         String value = array[index];
         if (value == null) {
-            return "Nothing";
+            return "0";
         }
         return value;
     }
+
+    public int[] getContiguousEmptyArea() {
+        int startPointer = 0;
+        int pointerOffset = 0;
+
+        for (int i = 0; i < size; i++) {
+            int tempPointerOffset = 0;
+            while (array[(i + tempPointerOffset) % size] == null) {
+                tempPointerOffset++;
+            }
+            if (tempPointerOffset > pointerOffset) {
+                startPointer = i;
+                pointerOffset = tempPointerOffset;
+            }
+        }
+
+        return new int[] {startPointer, pointerOffset};
+    }
+
     public int getOccupied() {
         return occupied;
     }
 
-    public int getSize(){
+    public int getSize() {
         return size;
+    }
+
+    public int[] getContiguousFilledArea() {
+        int startPointer = 0;
+        int pointerOffset = 0;
+
+        for (int i = 0; i < size; i++) {
+            int tempPointerOffset = 0;
+            while (!(array[(i + tempPointerOffset) % size] == null)) {
+                tempPointerOffset++;
+            }
+            if (tempPointerOffset > pointerOffset) {
+                startPointer = i;
+                pointerOffset = tempPointerOffset;
+            }
+        }
+
+        return new int[] {startPointer, pointerOffset};
+    }
+
+    public int[] getMaxHashOccurrence() {
+        int mostCommonHash = getMaxIndex(hashOccurrenceArray);
+
+        return new int[] {mostCommonHash, hashOccurrenceArray[mostCommonHash]};
+    }
+
+    private int getMaxIndex(int[] arr) {
+        int maxSumIndex = 0;
+        for (int i = 1; i < arr.length; i++) {
+            maxSumIndex = arr[i] > arr[maxSumIndex] ? i : maxSumIndex;
+        }
+        return maxSumIndex;
+    }
+
+    public Map.Entry<String, Integer> getFarthestInsert() {
+        int maxValue = (Collections.max(wordOffsetList.values()));
+        String key = "";
+        Map.Entry<String, Integer> farthestInsert = Map.entry("-1", -1);
+
+        for (Map.Entry<String, Integer> entry : wordOffsetList.entrySet()) {
+            if (entry.getValue() == maxValue) {
+                farthestInsert = entry;
+            }
+        }
+        return farthestInsert;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class Main {
@@ -29,34 +30,42 @@ public class Main {
         //Print out hashes of all 604 words
         hashes = Hash.hashStringsFromFile(edgarPath, edgarRegex);
         words = FileRead.parseValidWordsFromFile(edgarPath, edgarRegex);
-        //Print
-        //        System.out.println("Hashes from file:");
-        //        int perLine = 10;
-        //        for (int i = 0; i < hashes.size(); i++) {
-        //            if ((i % perLine == 0)) {
-        //                System.out.println();
-        //            }
-        //            System.out.print(hashes.get(i) + " ");
-        //        }
+        //print points given by doing problem 2
     }
 
     private static void hashTable() {
         //remove duplicates, preserving order
-        Set<String> set = new LinkedHashSet<>();
-        set.addAll(words);
-        List<String> uniqueWords = new ArrayList<>();
-        uniqueWords.addAll(set);
-        //table
+        Set<String> set = new LinkedHashSet<>(words);
+        List<String> uniqueWords = new ArrayList<>(set);
+        //create table
         EnglingHashTable hashTable = new EnglingHashTable(293);
         for (String word : uniqueWords) {
             hashTable.insert(word);
         }
+        //print table
+        System.out.printf("%-32s%-32s%-32s\n", "Hash Address", "Hashed Word", "Hash Value");
         for (int index = 0; index < hashTable.getSize(); index++) {
-            System.out.printf("",index, );
+            System.out.printf("%-32d%-32s%-32d\n", index, hashTable.getIndex(index),
+                              hashTable.getIndex(index).equals("0") ? -1 : Hash.hashString(hashTable.getIndex(index)));
         }
-        System.out.println(hashTable.getLoadFactor());
+        System.out.println(
+            "Occupied spaces: " + hashTable.getOccupied() + "\nLoad factor: " + hashTable.getLoadFactor());
+        int[] empty = hashTable.getContiguousEmptyArea();
+        System.out.println("The longest empty area starts at index: " + empty[0] + " and stops at (not including) " + (
+            empty[0] + empty[1]));
+        int[] full = hashTable.getContiguousFilledArea();
+        System.out.println(
+            "The longest non-empty area starts at index: " + full[0] + " and stops at (not including)" + " " + (
+                full[0] + full[1]));
+        int[] occur = hashTable.getMaxHashOccurrence();
+        System.out.println("The most common hash is: " + occur[0] + " with the value being " + occur[1]);
+        Map.Entry<String, Integer> farthestInsert = hashTable.getFarthestInsert();
+        System.out.println("The farthest insert is: " + farthestInsert.getKey() + " with the value being "
+                           + farthestInsert.getValue());
+
+
     }
 
-    private static void dijkstra() {
-    }
+    //    private static void dijkstra() {
+    //    }
 }
