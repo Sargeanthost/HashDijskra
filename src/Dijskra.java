ba91c200a95b55d2d10b75efc2512bbd0f2eb23a
Sargeanthost <sargeanthost@gmail.com>
2022-12-06 01:13:00 -0500

    dijskra heap allocation error

--- src/Dijskra.java
+++ src/Dijskra.java
@@ -1,21 +1,16 @@
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
 import java.util.PriorityQueue;
-import java.util.TreeMap;
 
 public class Dijskra {
     //TODO if time allows change this to read from a file
 
-    private final static int[][] graph =
+    private final int[][] englingGraph =
         {{0, 53, 10, 12, 0, 0, 0, 0, 0, 0}, {53, 0, 33, 0, 2, 0, 101, 0, 0, 0}, {10, 33, 0, 9, 30, 18, 0, 0, 0, 0},
             {12, 0, 9, 0, 0, 17, 0, 0, 6, 0}, {0, 2, 30, 0, 0, 14, 123, 122, 0, 0}, {0, 0, 18, 17, 14, 0, 0, 137, 7, 0},
             {0, 101, 0, 0, 123, 0, 0, 8, 0, 71}, {0, 0, 0, 0, 122, 137, 8, 0, 145, 66},
             {0, 0, 0, 6, 0, 7, 0, 145, 0, 212}, {0, 0, 0, 0, 0, 0, 71, 66, 212, 0}};
-    private static final int graphSize = 10;
+    private final int graphSize = 10;
 
     /**
      * Finds the shortest path from the source node to all the given nodes. The entry in the returned list reading 0
@@ -26,86 +21,147 @@ public class Dijskra {
      * @return returns a list of weights from the source node to each other node
      */
 
-    public static int[] dijskra(int[][] graph, int sourceNode, int graphSize) {
-        int[] finalDistances = new int[graphSize];//index is the node, value is the final shortest path from source
-        List<Map.Entry<Integer,Integer>> dumpList = new ArrayList<>();
-        PriorityQueue<Map.Entry<Integer, Integer>> pQueue =
-            new PriorityQueue<>((a,b) -> b.getValue() - a.getValue());//<node, distance>. sort low high
-
-        for (int i = 0; i < graphSize; i++) {
-            if (i == sourceNode) {
-                dumpList.add(i, Map.entry(sourceNode, 0));
+    public List<Node> englingDijskra(int[][] graph, int sourceNode, int destNode, int graphSize) {
+        //        List<Map.Entry<Integer, Integer>> dumpList = new ArrayList<>();
+        //        PriorityQueue<Map.Entry<Integer, Integer>> pQueue =
+        //            new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());//<node, distance>. sort low high
+        List<Node> listOfNodes = new ArrayList<>();
+        for (int row = 0; row < graphSize; row++) {
+            //            String name = switch (row) {
+            //                case 0 -> "A";
+            //                case 1 -> "J";
+            //                case 2 -> "M";
+            //                case 3 -> "R";
+            //                case 4 -> "K";
+            //                case 5 -> "S";
+            //                case 6 -> "I";
+            //                case 7 -> "N";
+            //                case 8 -> "T";
+            //                case 9 -> "D";
+            //                default -> throw new IllegalStateException("Unexpected value: " + row);
+            //            };
+            if (row == sourceNode) {
+                Node node = new Node(row, 0);
             }
-            dumpList.add(i, Map.entry(i,Integer.MAX_VALUE));
+            Node node = new Node(row, Integer.MAX_VALUE);
+            listOfNodes.add(node);
         }
-        pQueue.addAll(dumpList);
+        PriorityQueue<Node> priorityQueue = new PriorityQueue<>();
+        priorityQueue.addAll(listOfNodes);
 
-        while (pQueue.size() > 0) {
-            Map.Entry<Integer, Integer> nodeData = pQueue.poll();
-            finalDistances[nodeData.getKey()] = nodeData.getValue();
-
-            int nodeIndex = nodeData.getKey();
-            int nodeValue = nodeData.getValue();
-            int[] potentialNeighbors = graph[nodeIndex];
-//            Map.Entry<Integer,Integer>[] referenceQueue = (Map.Entry<Integer, Integer>[]) pQueue.toArray();//bleh, dont
-            // want to
-            // redo what i have
-            for (int i = 0; i < graphSize; i++) {
-                int potentialNeighborValue = graph[nodeIndex][i];
-                Map.Entry<Integer,Integer> workingEntry = dumpList.get(i); // more bleh
-//                for(Map.Entry<Integer,Integer> entry: referenceQueue){
-//                    if (entry.getKey() == i){
-//                        workingEntry = entry;//! ONLY FOR VALUE CHECKING
-//                    }
-//                }
-
-                //if not self, & me plus my connection is <less than your existing connection...
-                if(potentialNeighborValue != 0 && i != nodeIndex && nodeValue + potentialNeighborValue < workingEntry.getValue()){
-
+        while (priorityQueue.size() > 0) {
+            //TODO compare to destnode
+            Node currentNode = priorityQueue.poll();//current shortest. at start it will be sourceNode
+            currentNode.setHasBeenVisited();
+            for (int neighbor = 0; neighbor < graphSize; neighbor++) {
+                //current column, until column exhausted ("neighbor" = neighbor index, value = distance from me to u
+                Node possibleNeighborNode = listOfNodes.get(neighbor);
+                //my current distance plus my distance to you
+                int possibleDistance = graph[currentNode.getNodeNameIndex()][neighbor] + currentNode.getDistance();
+                if (!possibleNeighborNode.hasBeenVisited() && neighbor != currentNode.getNodeNameIndex()
+                    && possibleDistance < possibleNeighborNode.getDistance()) {
+                    possibleNeighborNode.setPrevious(currentNode);
+                    possibleNeighborNode.setDistance(possibleDistance);
+                    //reorder, since java has these methods private
+                    if (!priorityQueue.remove(listOfNodes.get(neighbor))) {
+                        System.out.println("Something went wrong removing previous old value for node in pQueue");
+                    }
+                    priorityQueue.add(listOfNodes.get(neighbor));
                 }
             }
+            if (currentNode.equals(listOfNodes.get(destNode))) {
+                backTrackPath(currentNode);
+                break;
+            }
         }
-        return finalDistances;
+        return backTrackPath(listOfNodes.get(destNode));
     }
 
-    public void run(int startingNode) {
+    List<Node> backTrackPath(Node node) {
+        List<Node> pathList = new ArrayList<>();
+
+        Node currentNode = node;
+        while (node != null) {
+            pathList.add(currentNode);
+            currentNode = node.getPrevious();
+        }
+        System.out.println();
+        return pathList;
+    }
+
+    public static String getNodeName(int index) {
+        return switch (index) {
+            case 0 -> "A";
+            case 1 -> "J";
+            case 2 -> "M";
+            case 3 -> "R";
+            case 4 -> "K";
+            case 5 -> "S";
+            case 6 -> "I";
+            case 7 -> "N";
+            case 8 -> "T";
+            case 9 -> "D";
+            default -> throw new IllegalStateException("Unexpected value: " + index);
+        };
+    }
+
+    public void run(int startingNode, int destNode) {
         if (startingNode >= graphSize || startingNode < 0) {
             System.out.println("Invalid starting node for engling graph!");
             return;
         }
-        dijskra(graph, startingNode, 10);
+        englingDijskra(englingGraph, startingNode, destNode, 10);
     }
 
-    //    class SmartPriorityQueue {
-    //
-    //        int size;//effective size
-    //        List<Map.Entry<Integer,Integer>> list;//node, weight
-    //
-    //        /**
-    //         * Queue made for dijskra. Initialize queue with graph.length elems, and set first entry in map to the
-    //         node,
-    //         * and the second to Infinity. When update is needed, will move map entry up based on the value of the
-    //         node
-    //         * key. This happens after each modification internally. {@link SmartPriorityQueue#pop} will return the
-    //         * map entry and delete it from the queue.
-    //         */
-    //        SmartPriorityQueue() {
-    //            size = 10;
-    //            arr = new int[size];
-    //            for (int index = 1; index < size; index++) {
-    //                arr[index] = initValue;
-    //            }
-    //        }
-    //
-    //        void modifyIndex(int node, int value){
-    //            for(Map.Entry<Integer, Integer> entry : list)
-    //
-    //            sort();
-    //        }
-    //
-    //        int get
-    //        void sort(){
-    //            Arrays.sort(arr);
-    //        }
-    //    }
+    class Node implements Comparable<Node> {
+        private Node previous;
+
+        private int nodeNameIndex;
+        private int distance;
+        private boolean hasBeenVisited = false;
+
+        Node(int nodeNameIndex, int distance) {
+            this.nodeNameIndex = nodeNameIndex;
+            this.distance = distance;
+        }
+
+        public int getNodeNameIndex() {
+            return nodeNameIndex;
+        }
+
+        void setHasBeenVisited() {
+            hasBeenVisited = true;
+        }
+
+        boolean hasBeenVisited() {
+            return hasBeenVisited;
+        }
+
+        void setDistance(int distance) {
+            this.distance = distance;
+        }
+
+        int getDistance() {
+            return distance;
+        }
+
+        void setPrevious(Node previous) {
+            this.previous = previous;
+        }
+
+        Node getPrevious() {
+            return previous;
+        }
+
+        @Override
+        public int compareTo(Node toCompare) {
+            int compare = this.distance - toCompare.getDistance();
+            if (compare == 0) {
+                return 0;
+            } else if (compare > 0) {
+                return 1;
+            }
+            return -1;
+        }
+    }
 }
